1926460d505f393ec5762b3f12ddb06ff0399932
이진규 <max322318@gmail.com>
2024-02-27 17:48:28 +0900

    feat: 괄호 회전하기 (stack)문제
    # stack으로 쌓이다가 제거될 조건 시, 문자열에서 제거 (닫히는 괄호일 때)
    
    # for(회전) - while (올바른 괄호들 제거)의 이중 구조
    
    # 인덱스를 활용한 슬라이싱을 적용했기 때문에 인덱스는 바로 이전으로 되돌림

--- rotate_brackets_jk/main.py
+++ rotate_brackets_jk/main.py
@@ -1,6 +1,40 @@
 def solution(s):
-    answer = -1
-    return answer
+    str = s
+    result = 0
+    # 글자 돌리기
+
+    for i in range(len(s)):
+        idx = 0
+        while idx < len(str):
+            if idx == 0 and (str[idx] == ']' or str[idx] == '}' or str[idx] == ')'):
+                break
+
+            if str[idx] == ']':
+                if '[' == str[idx-1]:
+                    str = str[:idx-1] + str[idx+1:]
+                    idx -= 1
+                    continue
+
+            elif str[idx] == '}':
+                if '{' == str[idx-1]:
+                    str = str[:idx-1] + str[idx+1:]
+                    idx -= 1
+                    continue
+
+            elif str[idx] == ')':
+                if '(' == str[idx-1]:
+                    str = str[:idx-1] + str[idx+1:]
+                    idx -= 1
+                    continue
+
+            idx += 1
+
+        if not str:
+            result += 1
+
+        str = s[i+1:len(s)] + s[:i+1]
+
+    return result
 
 if __name__ == '__main__':
-    print(solution("[](){}"))
+    print(solution("[)(]"))
