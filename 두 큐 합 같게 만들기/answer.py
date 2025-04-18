a3101500b3c32879db218aa5af03a3a69f424cfe
jingyulee <max322318@gmail.com>
2025-04-18 14:09:53 +0900

    programmers :: 두 수의 합 같게 하기

--- 두 큐 합 같게 만들기/answer.py
+++ 두 큐 합 같게 만들기/answer.py
@@ -2,36 +2,33 @@ from collections import deque
 def solution(queue1, queue2):
     q1 = deque(queue1)
     q2 = deque(queue2)
-    turn = 0
-    sum1, sum2 = sum(q1), sum(q2)
+
+    sum1 = sum(q1)
+    sum2 = sum(q2)
     total = sum1 + sum2
 
-    # 반쪽자리 합만 맞추면 되므로
     if total % 2 != 0:
-        return -1
+        return -1  # 홀수는 나눌 수 없음
+
     target = total // 2
+    count = 0
+    max_count = len(queue1) * 3
 
-    # 이정도까지만 탐색
-    safe_boundary = len(q1)*3
-
-    while turn < safe_boundary:
+    # 어짜피 한쪽의 queue에서 반쪽합만 만족하면 되면 그렇게 구현
+    while count <= max_count:
         if sum1 == target:
-            return turn
-
+            return count
         elif sum1 > target:
             num = q1.popleft()
-            q2.append(num)
             sum1 -= num
-
+            q2.append(num)
         else:
             num = q2.popleft()
-            q1.append(num)
             sum1 += num
-
-        turn += 1
+            q1.append(num)
+        count += 1
 
     return -1
-
 
 if __name__ == "__main__":
     queue1 = [3, 2, 7, 2]
