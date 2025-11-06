85aa14be424f157927c758146dbcd266c853595f
jingyulee <max322318@gmail.com>
2025-11-07 00:37:19 +0900

    cs스터디 :: 트리, 트리 순회 골드

--- cs_study/트리/트리 순회 골드/main.py
+++ cs_study/트리/트리 순회 골드/main.py
@@ -3,16 +3,28 @@
 """
 import sys
 sys.setrecursionlimit(10**7)
-def dfs(cur_node, parent_node, visited, total_cnt):
+
+def dfs(cur_node, visited, total_cnt):
     visited[cur_node] = True
-    for next_node in adj_dict[cur_node]:
-        #print(cur_node, total_cnt[0], visited)
-        total_cnt[0] += 1
-        dfs(next_node, cur_node, visited, total_cnt)
-        if all(visit for visit in visited):
-            return
-        total_cnt[0] += 1
-        # print(cur_node, total_cnt[0], visited)
+    if all(visit for visit in visited):
+        return
+
+    total_cnt[0] += 1
+    if cur_node == 0:
+        return
+
+    left, right = adj_dict[cur_node]
+
+    dfs(left, visited, total_cnt)
+    if all(visit for visit in visited):
+        return
+    total_cnt[0] += 1
+
+    dfs(right, visited, total_cnt)
+    if all(visit for visit in visited):
+        return
+    total_cnt[0] += 1
+
 
 def solution(N, adj_dict):
     # 루트 노드 찾을 때는 set으로 찾기
@@ -24,27 +36,29 @@ def solution(N, adj_dict):
 
     root_node = list(all_node - child_node)
     root = root_node[0]
-    print()
-    print(root)
-
 
     visited = [False] * (N+1)
     visited[0] = True
     total_cnt = [0]
-    dfs(root, root, visited, total_cnt)
+    dfs(root, visited, total_cnt)
     return total_cnt[0]
 
 if __name__ == "__main__":
     N = int(input())
-    adj_dict = {
-        node: [] for node in range(1, N+1)
-    }
+    adj_dict = {}
     for _ in range(N):
         parent, child1, child2 = map(int, input().split())
-        if child1 != -1:
-            adj_dict[parent].append(child1)
+        if child1 != -1 and child2 != -1:
+            adj_dict[parent] = (child1, child2)
 
-        if child2 != -1:
-            adj_dict[parent].append(child2)
+        else:
+            if child1 == -1 and child2 == -1:
+                adj_dict[parent] = (0, 0)
+
+            elif child1 == -1:
+                adj_dict[parent] = (0, child2)
+
+            elif child2 == -1:
+                adj_dict[parent] = (child1, 0)
 
     print(solution(N, adj_dict))
