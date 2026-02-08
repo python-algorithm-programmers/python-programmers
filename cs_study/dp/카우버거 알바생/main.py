ccd413033d48137af6374d1ff645b8a0024c21c7
jingyulee <max322318@gmail.com>
2026-02-09 00:43:21 +0900

    cs스터디 :: dp, 카우버거 알바생

--- cs_study/dp/카우버거 알바생/main.py
+++ cs_study/dp/카우버거 알바생/main.py
@@ -1,9 +1,30 @@
 def solution(N, M, K, orders):
-    dp = [[0]*(N+1) for _ in range(N+1)]
-    for cheeze, potato in orders:
+    orders.sort(key=lambda x:(x[0], x[1]))
 
+    # 현재 사용한 치즈버거, 감자 튀김 기반 기준 최대 주문 수
+   # 현재 사용한 치즈버거, 감자 튀김 -> 최대 주문수
+    dp = {
+        (0, 0): 0
+    }
+    for cheese, potato in orders:
+        next_dp = dict(dp)
+        #print(cheese, potato)
+        #print("before next_dp")
+        #print(next_dp)
 
-    return max(dp)
+        for (c, p), order_cnt in dp.items():
+            next_c, next_p = c+cheese, p+potato
+            if next_c <= M and next_p <= K:
+                next_dp[(next_c, next_p)] = max(
+                    next_dp.get((next_c, next_p), 0),
+                    order_cnt + 1
+                )
+        dp = next_dp
+        #print("next next_dp")
+        #print(next_dp)
+        #print()
+
+    return max(dp.values())
 
 if __name__ == "__main__":
     import sys
